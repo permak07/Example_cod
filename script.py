889951b062690bd90ecce22239102c28e0ef429b
Nikita Permyakov <permakovnikita70@gmail.com>
2026-02-11 16:09:08 +0800

    Add zero division check

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 def add(a,b):
 	return a+b
 def divistion(a,b):
+	if b==0:
+		raise ValueError("Zero division is not allowed")
 	return a/b
 
 print(add(30,10))
-print(divistion(100,5))
+print(divistion(100,0))
