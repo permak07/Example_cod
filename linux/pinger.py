2172a9c20136fcbe74bf08dbb3efbfe51374228c
Nikita Permyakov <permakovnikita70@gmail.com>
2026-02-25 16:26:58 +0800

    Add source right date pinger.py

--- linux/pinger.py
+++ linux/pinger.py
@@ -1,10 +1,16 @@
 import requests
-from datetime import datetime
+from datetime import datetime,timedelta
 
 url_teamplate="https://simurg.space/gen_file?data=obs&date={date}"
 now=datetime.now()
-date=now.strftime("%Y-%m-%d")
-url=url_teamplate.format(date=date)
+while True:
+    date=now.strftime("%Y-%m-%d")
+    url=url_teamplate.format(date=date)
 
-response=requests.get(url=url,stream=True)
-print(f"for {date} got: {response}")
+    response=requests.get(url=url,stream=True)
+    print(f"for {date} got: {response}")
+    if response.status_code ==200:
+        print(f"last available data are for {date}")
+        break
+    else:
+        now=now-timedelta(days=1)
