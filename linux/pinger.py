0c0f19addba8efdb3fcc1af3eecf75c0c08cbbe4
Nikita Permyakov <permakovnikita70@gmail.com>
2026-02-25 16:30:49 +0800

    Refactor  pinger.py

--- linux/pinger.py
+++ linux/pinger.py
@@ -2,9 +2,9 @@ import requests
 from datetime import datetime,timedelta
 
 url_teamplate="https://simurg.space/gen_file?data=obs&date={date}"
-now=datetime.now()
+current=datetime.now()
 while True:
-    date=now.strftime("%Y-%m-%d")
+    date=current.strftime("%Y-%m-%d")
     url=url_teamplate.format(date=date)
 
     response=requests.get(url=url,stream=True)
@@ -13,4 +13,4 @@ while True:
         print(f"last available data are for {date}")
         break
     else:
-        now=now-timedelta(days=1)
+        current=current-timedelta(days=1)
