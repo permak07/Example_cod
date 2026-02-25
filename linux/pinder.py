232240a6c19fe7ac3c90c748c1fd09b5df581df6
Nikita Permyakov <permakovnikita70@gmail.com>
2026-02-25 16:13:52 +0800

    Modification  pinder.py

--- linux/pinder.py
+++ linux/pinder.py
@@ -1,6 +1,9 @@
 import requests
+from datetime import datetime
+
 url_teamplate="https://simurg.space/gen_file?data=obs&date={date}"
-date="2026-02-19"
+now=datetime.now()
+date=now.strftime("%Y-%m-%d")
 url=url_teamplate.format(date=date)
 
 response=requests.get(url=url,stream=True)
