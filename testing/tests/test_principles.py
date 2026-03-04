a2108d332d92d0556417f73fbac685d9a3afa9e6
Nikita Permyakov <permakovnikita70@gmail.com>
2026-03-04 17:01:57 +0800

    Add test_calculate and calculate_tax

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -2,7 +2,10 @@
 # sys.path.append("../src")
 # #TODO make it with pip install -e
 from src.math_demo import (add, 
-                           add_with_bug)
+                           add_with_bug,
+                           calculate_tax_with_bug,
+                           calculate_tax
+                           )
 def test_addition():
     assert add(2,2)==4
     print("Test basic addition")
@@ -14,6 +17,7 @@ def test_addition_with_bug():
 
 def test_addition_duplicated():
     assert add(2,3)==3+2
+    print("test_additoin_duplicated")
 
 # bad test
 def test_addition_overcomplicatid():
@@ -23,6 +27,7 @@ def test_addition_overcomplicatid():
             assert add(-i,j)==sum(-i,j)
             assert add(i,-j)==sum(i,-j)
             assert add(-i,-j)==sum(-i,-j)
+
 def test_addition_reasonable():
     assert add(2,2)==4
     assert add(0,0)==0
@@ -38,6 +43,30 @@ def test_additon_communication():
     assert add(-6,7)==1
     print("test_addition_communication")
 
+def test_calculate_pesticised():
+    assert calculate_tax_with_bug(1000)==150
+    assert calculate_tax_with_bug(100)==15
+    assert calculate_tax_with_bug(10)==1.5
+    assert calculate_tax_with_bug(1)==0.15
+    assert calculate_tax_with_bug(245)==36.75
+    assert calculate_tax_with_bug(-200)==-30
+    assert calculate_tax_with_bug(0)==0
+    print("test_calculate_pesticised")
+    # Неточность
+    #assert calculate_tax_with_bug(24.5)==3.67 #3.675
+
+def test_calculate():
+    assert calculate_tax_with_bug(1000)==150
+    assert calculate_tax_with_bug(100)==15
+    assert calculate_tax_with_bug(10)==1.5
+    assert calculate_tax_with_bug(1)==0.15
+    assert calculate_tax_with_bug(245)==36.75
+    assert calculate_tax_with_bug(-200)==-30
+    assert calculate_tax_with_bug(0)==0
+    assert calculate_tax_with_bug(24.5)==3.67 #3.675
+    print("test_calculate")
+
+
 if __name__=="__main__":
     test_addition()
     test_addition_with_bug()
@@ -45,3 +74,5 @@ if __name__=="__main__":
     #test_addition_overcomplicatid()
     test_addition_reasonable()
     test_additon_communication()
+    test_calculate_pesticised()
+    test_calculate()
