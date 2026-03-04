e3631ff3b273db83283ee2a3dbbae098b690ad91
Nikita Permyakov <permakovnikita70@gmail.com>
2026-03-04 16:28:52 +0800

    Add bad test_overcomplicated

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -15,6 +15,17 @@ def test_addition_with_bug():
 def test_addition_duplicated():
     assert add(2,3)==3+2
 
+# bad test
+def test_addition_overcomplicatid():
+    for i in range(0,2**32):
+        for j in range(0,2**32):
+            assert add(i,j)==sum(i,j)
+            assert add(-i,j)==sum(-i,j)
+            assert add(i,-j)==sum(i,-j)
+            assert add(-i,-j)==sum(-i,-j)
+
 if __name__=="__main__":
     test_addition()
     test_addition_with_bug()
+    test_addition_duplicated()
+    #test_addition_overcomplicatid()
