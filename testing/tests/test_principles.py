af8a7255b47bd017aa28b105e277642513ca5b02
Nikita Permyakov <permakovnikita70@gmail.com>
2026-03-04 16:15:45 +0800

    Add addition_with_bug

--- testing/tests/test_principles.py
+++ testing/tests/test_principles.py
@@ -1,10 +1,17 @@
 import sys
 sys.path.append("../src")
-#TODO make it with pip install -c
-from math_demo import add
+#TODO make it with pip install -e
+from src.math_demo import (add, 
+                           add_with_bug)
 def test_addition():
     assert add(2,2)==4
     print("Test basic addition")
 
+def test_addition_with_bug():
+    assert add_with_bug(2,2)==4
+    print("Test bug addition_with_bug")
+    assert add_with_bug(6,7)==13 #fail
+
 if __name__=="__main__":
     test_addition()
+    test_addition_with_bug()
