fc2084a4a27f55d0bf0f9eb46791b190f1fcbfcb
Nikita Permyakov <permakovnikita70@gmail.com>
2026-03-11 17:03:16 +0800

    Update ndfl and 2 tests

--- testing_tdd/src/ndfl.py
+++ testing_tdd/src/ndfl.py
@@ -1,2 +1,10 @@
 def calculate_ndfl_lax(param):
-    return param * 0.13
+    tier=[
+        (0.0,0.0,0.13),
+        (2_400_000.0,312_000.0, 0.15),
+        (5_000_000.0,762_000.0,0.18),
+          ]
+    for start,addition, rate in tier:
+        if param>start:
+            return addition+(param-start)*rate
+    return
