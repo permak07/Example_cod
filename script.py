c02fd2aeda9e023869061101f6cfc14a86211b4f
Nikita Permyakov <permakovnikita70@gmail.com>
2026-04-01 16:51:50 +0800

    Update func _parse_record

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
-def add(a,b):
-	return a+b
-def divistion(a,b):
-	if b==0:
-		raise ValueError("Zero division is not allowed")
-	return a/b
+def add(a, b):
+    return a + b
 
-print(add(30,10))
-print(divistion(100,0))
+
+def division(a, b):
+    if b == 0:
+        raise ValueError("Zero division is not allowed")
+    if isinstance(a, str) or isinstance(b, str):
+        raise ValueError("String division is not allowed")
+    return a / b
